81d0b62c8b7bc1e3ec77c7867ab6b1c1fdd01958
凰 陈 <chenhuang45@qq.com>
2024-10-02 22:25:15 +0800

    20241002： 添加了 液化 主动技能 硬化皮肤 被动技能 秩序 被动技能 罪恶值系统 buff设定

---
+++ buff/硬化皮肤_被动.cs
@@ -0,0 +1,55 @@
+﻿namespace 战斗小游戏
+{
+    /// <summary>
+    /// Author: 陈
+    /// Description: 硬化皮肤被动部分
+    /// </summary>
+    class 硬化皮肤_被动 : Buff
+    {
+        public 硬化皮肤_被动(string 创建者, int 技能倍率)
+        {
+            Name = "硬化皮肤";
+            UUID = "受到的伤害减少";
+            this.创建者 = 创建者;
+            可以被驱散 = false;
+            是正面buff = true;
+            是负面buff = false;
+            MagicNumber = 技能倍率;
+        }
+
+        public override void 受到物理伤害效果(角色 buff持有者, DamageInfo 攻击事件)
+        {
+            //提取受击事件中的伤害量
+            伤害效果 damage = 攻击事件.GetDamage();
+
+            //暂存一个伤害减免前的伤害量
+            int text = damage.Value;
+
+            damage.Value = damage.Value * (100 - MagicNumber) / 100;
+            Console.WriteLine($"硬化皮肤效果使得 {buff持有者.Name} 受到的伤害减少了  {text - damage.Value}");
+        }
+
+        public override void 受到法术伤害效果(角色 buff持有者, DamageInfo 攻击事件)
+        {
+            //提取受击事件中的伤害量
+            伤害效果 damage = 攻击事件.GetDamage();
+
+            //暂存一个伤害减免前的伤害量
+            int text = damage.Value;
+
+            damage.Value = damage.Value * (100 - MagicNumber) / 100;
+            Console.WriteLine($"硬化皮肤效果使得 {buff持有者.Name} 受到的伤害减少了  {text - damage.Value}");
+        }
+
+        public override void 重复施加(Buff b)
+        {
+            // 对比持续回合，保留最大的，并修改创建者
+            if (b.持续回合 > this.持续回合)
+            {
+                Console.WriteLine($"来自{b.创建者}的伤害减免效果覆盖了{this.创建者}的伤害减免效果");
+                this.持续回合 = b.持续回合;
+                this.创建者 = b.创建者;
+            }
+        }
+    }
+}
---
+++ 技能/液化.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 战斗小游戏
+{
+    /// <summary>
+    /// Author: 陈
+    /// Description: 液化主动技能：为友军提供3次目标随机100%法术强度的恢复
+    /// </summary>
+    internal class 液化 : 技能
+    {
+        public 液化()
+        {
+            Name = "液化";
+            技能描述 = "为友军提供3次目标随机100%法术强度的生命恢复";
+            是主动技能 = true;
+            有效目标 = 允许目标.友方全体;
+            倍率 = 100;
+        }
+
+        public override void 使用技能(角色 释放者, 角色[] 目标)
+        {
+
+            Console.WriteLine($"{释放者.Name} 使用了 {Name}");
+
+            int 治疗量 = 释放者.法术强度 * 倍率 / 100;
+
+            //提取友方单位数组
+            目标 = 战斗管理器.GetInstance().友方;
+
+            //创建随机数实例
+            Random rand = new Random();
+            //创建存储3个选择目标的角色数组
+            角色[] rollName = new 角色[3];
+
+            //进行三次循环选择对象
+            for (int i = 0; i < 3; i++)
+            {
+                rollName[i] = 目标[rand.Next(目标.Length)];//选择治疗单位
+            }
+
+            foreach (var roll in rollName) //对已选出的目标进行顺序治疗
+            {
+                //技能文本
+                string message = $"{roll.Name} 受到 {释放者.Name} {治疗量}点治疗";
+
+                //将释放对象角色转换为角色数组
+                角色[] rollNamei = new 角色[] { roll };
+                //治疗目标重定义到已选定的三位友方单位之一
+                目标 = rollNamei;
+
+                //进行治疗流程
+                伤害效果 液化 = new 伤害效果(释放者, 目标, Name, message, 伤害类型.治疗, 攻击类型.技能伤害, 治疗量);
+                战斗管理器.GetInstance().处理伤害事件(new DamageInfo(液化));
+
+            }
+
+        }
+    }
+}
