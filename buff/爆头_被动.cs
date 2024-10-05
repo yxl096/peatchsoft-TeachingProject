9586fc8693ed5a7cd3f6ede9421d33ab87dceb70
Luo-XH <3149182697@qq.com>
2024-10-05 20:09:14 +0800

    20241005 把易伤buff分为物理易伤和法术脆弱 新增瞄准和瞄准标记(buff) 新增漏一个小头都得死及其被动（未完成）

---
+++ buff/爆头_被动.cs
@@ -0,0 +1,25 @@
+﻿
+namespace 战斗小游戏.buff
+{
+     class 爆头_被动 : Buff
+     {
+        public 爆头_被动(string 创建者, int 技能倍率) 
+        {
+            UUID = "攻击有瞄准状态的敌军时伤害增加300%";
+            this.创建者 = 创建者;
+            可以被驱散 = false;
+            是正面buff = false;
+            是负面buff = false;
+            MagicNumber = 技能倍率;
+        }
+        public override bool 发动普通攻击效果(角色 buff持有者, DamageInfo 攻击事件)
+        {
+            伤害效果 damage = 攻击事件.GetDamage();
+            if (damage.承受者.buff池.GetBuff(瞄准_buff, b))
+            {
+
+            }
+            return true;
+        }
+    }
+}
